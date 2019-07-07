b68bcc9b2f28406dddb360aa3554830c305ca9a7
homei-leung <49667943+homei-leung@users.noreply.github.com>
2019-07-06 22:20:04 -0400

    Update script.js
    Added activity section.

--- js/script.js
+++ js/script.js
@@ -22,32 +22,75 @@ $('#title').change(function(){
   }
 })
 
-
+/***
+T-Shirt section - initial load.
+Hide the "Select Theme" option element,
+update the "Color" field to read "Please select a T-shirt theme",
+hide the colors in the "Color" drop down menu.
+***/
+$("#design").find("option:eq(0)").hide();
+$("#color").prepend('<option disabled selected value="">Please select a T-shirt theme</option>');
+$("#color").children().hide();
 
 /***
-T-Shirt section.
+T-Shirt section - theme selection.
+Only appropriate colors show in "Color" drop down menu.
+"Color" field updates to the first available color.
 ***/
+$("#design").change(function(){
+  if($(this).val() == 'js puns'){
+    $("#color").children().hide();
+    $("#color").find("option:eq(1)").show();
+    $("#color").find("option:eq(2)").show();
+    $("#color").find("option:eq(3)").show();
+    $("#color").val('cornflowerblue');
+  }
+  if($(this).val() == 'heart js'){
+    $("#color").children().hide();
+    $("#color").find("option:eq(4)").show();
+    $("#color").find("option:eq(5)").show();
+    $("#color").find("option:eq(6)").show();
+    $("#color").val('tomato');
+  }
+});
 
 /***
 Activity section.
-***/
-
-
-/***
 Create an element to display the total activity cost.
+Create a DOM element and store in a global variable appended to '.activity'
+Create a global variable to store total activity cost initially set to 0.
 ***/
+let activity = document.createElement('p');
+let totalCost = 0;
+activity.innerHTML = "Total Activity Cost: " + totalCost;
+$(".activities").append(activity);
 
 /***
 Listening for changes in the activity section.
 ***/
-
-/***
-Update and display the total activity cost part 1.
-***/
-
-/***
-Update and display the total activity cost part 2.
-***/
+$("input[type='checkbox']").click(function(event){
+  let clickedInput = $(event.target);
+  let checkedlabel = clickedInput.parent().text();
+  /***
+  Update and display the total activity cost part 1.
+  Index of the dollar sign in the label text from the variable above.
+  Cost of the activity that was just clicked. Turn Cost string to numbers.
+  ***/
+  let dollarSign = checkedlabel.indexOf("$");
+  let labelCost = checkedlabel.slice(-3);
+  let selectedCost = parseFloat(labelCost);
+  /***
+  Update and display the total activity cost part 2.
+  Check if the clicked imput element is checked and add the cost of the activity to the total cost.
+  ***/
+  if (clickedInput.is(":checked")){
+    totalCost += selectedCost;
+  }
+  else {
+    totalCost -= selectedCost;
+  }
+  console.log(totalCost);
+});
 
 /***
 Disabling conflicting activities part 1.
